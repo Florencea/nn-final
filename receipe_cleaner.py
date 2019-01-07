071faab3e1694684152f211d325a78ed697adb57
Florencea <bearflorencea@gmail.com>
2019-01-07 16:08:05 +0800

    fix(receipe_cleaner.py): fix regexp rules

--- receipe_cleaner.py
+++ receipe_cleaner.py
@@ -4,13 +4,16 @@ import re
 import sys
 
 receipe = json.load(codecs.open(sys.argv[1], 'r', 'utf-8-sig'))
-regexp = '[\s+\.\!_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）(){}~`-【】：]+'
+regexp = '[\s+\.\/\!_,$%^*(+\"\']+|[+——！，。？?、~@#￥%……&*（）(){}~`-【】：:～><＞＜]+'
 
 for rc in receipe:
     rc['name'] = re.sub(regexp, ' ', rc['name'])
+    rc['name'] = re.sub(' +', ' ', rc['name'])
     rc['intro'] = re.sub(regexp, ' ', rc['intro'])
+    rc['intro'] = re.sub(' +', ' ', rc['intro'])
     for st in rc['steps']:
         st['content'] = re.sub(regexp, ' ', st['content'])
+        st['content'] = re.sub(' +', ' ', st['content'])
 
 with open(sys.argv[1].split('.')[0] + '_cleared.json', 'w') as json_file:
     json.dump(receipe, json_file)
